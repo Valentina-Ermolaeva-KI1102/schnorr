2f6f0e3e1f3587a8e7a32cde5676f6c8e06f278c
Valentina-Ermolaeva-KI1102 <luna650@mail.ru>
2014-12-25 07:03:51 +0700

    Create shnorr.h

---
+++ shnorr.h
@@ -0,0 +1,5 @@
+//#pragma comment(lib, "bigIntDll.lib")
+#include "bigInt.h"
+
+void GeneratedKey (char *p_fname);
+void AuProtocol(char *p_fname,char *q_fname,char *g_fname,char *y_fname, char *w_fname);
